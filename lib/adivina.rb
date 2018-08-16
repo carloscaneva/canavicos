5eaa0c0b1fc2aa78059e5fcd5599314cb241513a
mbertinat <martinbertinat10@gmail.com>
2018-08-16 19:04:10 +0200

    correccion

--- lib/adivina.rb
+++ lib/adivina.rb
@@ -1,10 +1,11 @@
 class Adivina
-	attr_reader :intentos_maximos, :intentos_realizados
+	attr_reader :intentos_maximos, :intentos_realizados, :palabra
 	attr_writer :intentos_realizados
 
 	def initialize()
 		@intentos_maximos = 6  
   		@intentos_realizados = 0 
+		@palabra = "a"
 	end	
 	def validarLetra(letra)
             if (letra.upcase == "A")
